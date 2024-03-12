bbb386ca3cf23c8bc3005ef646325fe9c421ec3a
Prox747 <carloproserpio1@gmail.com>
2024-03-13 00:44:22 +0100

    point b works

--- recommendation-fairness/main.py
+++ recommendation-fairness/main.py
@@ -1,71 +1,81 @@
 import pandas as pd
+import random as rand
+import utils
 import os
 
-# Ottieni il percorso della cartella del progetto
-project_folder = os.path.dirname(__file__)
 
+class RecommendationAssignment:
 
-# Costruisci i percorsi completi dei file CSV
-links_file_path = os.path.join(project_folder, 'ml-latest-small', 'links.csv')
-movies_file_path = os.path.join(project_folder, 'ml-latest-small', 'movies.csv')
-ratings_file_path = os.path.join(project_folder, 'ml-latest-small', 'ratings.csv')
-tags_file_path = os.path.join(project_folder, 'ml-latest-small', 'tags.csv')
+    def __init__(self):
+        self.project_folder = os.path.dirname(__file__)
 
+        self.create_data_frames()
+        self.print_df_heads()
 
-# Leggi i file CSV
-links_df = pd.read_csv(links_file_path)
-movies_df = pd.read_csv(movies_file_path)
-ratings_df = pd.read_csv(ratings_file_path)
-tags_df = pd.read_csv(tags_file_path)
+        self.users_ratings_mean = self.calc_users_ratings_mean()
+        self.user_movie_ratings = self.create_user_ratings_groups()
 
-# Visualizza le prime righe di ciascun DataFrame
-# e mostra il numero delle righe per ciascun DataFrame
-print("\n-------- LINKS.CSV --------")
-print("Numero di elementi in links.csv:", len(links_df))
-print("Primi elementi di links.csv:")
-print(links_df.head())
-
-
-print("\n-------- MOVIES.CSV --------")
-print("Numero di elementi in movies.csv:", len(movies_df))
-print("Primi elementi di movies.csv:")
-print(movies_df.head())
-
-print("\n-------- RATINGS.CSV --------")
-print("Numero di elementi in ratings.csv:", len(ratings_df))
-print("Primi elementi di ratings.csv:")
-print(ratings_df.head())
-
-print("\n-------- TAGS.CSV --------")
-print("Numero di elementi in tags.csv:", len(tags_df))
-print("Primi elementi di tags.csv:")
-print(tags_df.head())
+        print(utils.pearson_correlation(self.user_movie_ratings, self.users_ratings_mean, 45, 89))
 
 
 
+    
+    def create_data_frames(self):
+        # Costruisci i percorsi completi dei file CSV
+        links_file_path = os.path.join(self.project_folder, 'ml-latest-small', 'links.csv')
+        movies_file_path = os.path.join(self.project_folder, 'ml-latest-small', 'movies.csv')
+        ratings_file_path = os.path.join(self.project_folder, 'ml-latest-small', 'ratings.csv')
+        tags_file_path = os.path.join(self.project_folder, 'ml-latest-small', 'tags.csv')
+
+        # Leggi i file CSV
+        self.links_df = pd.read_csv(links_file_path)
+        self.movies_df = pd.read_csv(movies_file_path)
+        self.ratings_df = pd.read_csv(ratings_file_path)
+        self.tags_df = pd.read_csv(tags_file_path)
+
+    # Prints the first few rows of each table
+    def print_df_heads(self):
+        print("\n-------- LINKS.CSV --------")
+        print("Number of elements in links.csv:", len(self.links_df))
+        print("First few rows of links.csv:")
+        print(self.links_df.head())
 
 
+        print("\n-------- MOVIES.CSV --------")
+        print("Number of elements in movies.csv:", len(self.movies_df))
+        print("First few rows of movies.csv:")
+        print(self.movies_df.head())
 
-# Group the DataFrame by user ID to obtain ratings for each user
-grouped_by_user = ratings_df.groupby('userId')
+        print("\n-------- RATINGS.CSV --------")
+        print("Number of elements in ratings.csv:", len(self.ratings_df))
+        print("First few rows of ratings.csv:")
+        print(self.ratings_df.head())
 
-# Create a dictionary where the keys are movie IDs and the values are tuples containing ratings for both users
-user_ratings = {}
-for user_id, group in grouped_by_user:
-    ratings = dict(zip(group['movieId'], group['rating']))
-    user_ratings[user_id] = ratings
+        print("\n-------- TAGS.CSV --------")
+        print("Number of elements in tags.csv:", len(self.tags_df))
+        print("First few rows of tags.csv:")
+        print(self.tags_df.head())
+    
 
-# Filter out movies for which both users have provided ratings
-# Assuming you have two specific users, UserA and UserB
-user_a_ratings = user_ratings.get(1, {})
-user_b_ratings = user_ratings.get(2, {})
-common_movies = set(user_a_ratings.keys()) & set(user_b_ratings.keys())
+    # Calculates the mean of ratings for each users
+    def calc_users_ratings_mean(self):
+        grouped_by_user = self.ratings_df.groupby('userId')
 
-# Create the map movie:(ratingUserA, ratingUserB) for common movies
-movie_ratings_map = {}
-for movie_id in common_movies:
-    movie_ratings_map[movie_id] = (user_a_ratings.get(movie_id), user_b_ratings.get(movie_id))
+        return grouped_by_user.rating.mean()
 
-print("\n\n")
-print(movie_ratings_map)
 
+    def create_user_ratings_groups(self):
+        # Groups the DataFrame by user ID to obtain ratings for each user
+        grouped_by_user = self.ratings_df.groupby('userId')
+
+        # Creates a dictionary where the keys are movie IDs and the values are tuples containing ratings
+        user_ratings = {}
+        for user_id, user_df in grouped_by_user:
+            ratings = dict(zip(user_df['movieId'], user_df['rating']))
+            user_ratings[user_id] = ratings
+        
+        return user_ratings
+
+
+if __name__ == "__main__":
+    RecommendationAssignment()
--- recommendation-fairness/utils.py
+++ recommendation-fairness/utils.py
@@ -1,9 +1,42 @@
-import numpy
+import random
+import pandas as pd
+import math
 
-def pearson_correlation():
-    
-    
-    similarity = 0
+# Calculates Pearson Correlation
+def pearson_correlation(user_ratings: dict, users_ratings_mean: pd.Series, userA: int, userB: int):
+    # Creates a map containing all common movies between the users and their ratings for each movie
+    common_movies_with_ratings = create_movie_ratings_map(user_ratings, userA, userB)
+    userAMean = users_ratings_mean.get(userA)
+    userBMean = users_ratings_mean.get(userB)
+
+    numerator = 0
+    sum_of_squared_differences_B = 0 
+    sum_of_squared_differences_A = 0
+
+    # {MOVIEID, (RatingUserA, RatingUserB)}
+    for ratings in common_movies_with_ratings.values():
+        numerator += (ratings[0] - userAMean)*(ratings[1] - userBMean)
+        sum_of_squared_differences_A += (ratings[0] - userAMean) ** 2
+        sum_of_squared_differences_B += (ratings[1] - userBMean) ** 2
+        
+    denominator = (math.sqrt(sum_of_squared_differences_A)) * (math.sqrt(sum_of_squared_differences_B))
+
+    similarity = numerator / denominator
+
     return similarity
 
 
+# Given two users, it creates a map {MOVIEID, (RatingUserA, RatingUserB)}
+# Useful for calculating Pearson correlation (Calculates similarity between two users)
+def create_movie_ratings_map(user_ratings: dict, userA: int, userB: int):
+    # Create a set containing movies rated by both users
+    user_a_ratings = user_ratings.get(userA, {})
+    user_b_ratings = user_ratings.get(userB, {})
+    common_movies = set(user_a_ratings.keys()) & set(user_b_ratings.keys())
+
+    # Create the map {MOVIEID, (RatingUserA, RatingUserB)}
+    movie_ratings_map = {}
+    for movie_id in common_movies:
+        movie_ratings_map[movie_id] = (user_a_ratings.get(movie_id), user_b_ratings.get(movie_id))
+
+    return movie_ratings_map
