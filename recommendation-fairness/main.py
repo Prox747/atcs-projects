0b276a804e000fc8156b2acac3e9587e69885135
Carlo <car.proserpio@stud.uniroma3.it>
2024-03-12 15:58:51 +0100

    Began point b

--- recommendation-fairness/main.py
+++ recommendation-fairness/main.py
@@ -18,7 +18,6 @@ movies_df = pd.read_csv(movies_file_path)
 ratings_df = pd.read_csv(ratings_file_path)
 tags_df = pd.read_csv(tags_file_path)
 
- 
 # Visualizza le prime righe di ciascun DataFrame
 # e mostra il numero delle righe per ciascun DataFrame
 print("\n-------- LINKS.CSV --------")
@@ -41,3 +40,32 @@ print("\n-------- TAGS.CSV --------")
 print("Numero di elementi in tags.csv:", len(tags_df))
 print("Primi elementi di tags.csv:")
 print(tags_df.head())
+
+
+
+
+
+
+# Group the DataFrame by user ID to obtain ratings for each user
+grouped_by_user = ratings_df.groupby('userId')
+
+# Create a dictionary where the keys are movie IDs and the values are tuples containing ratings for both users
+user_ratings = {}
+for user_id, group in grouped_by_user:
+    ratings = dict(zip(group['movieId'], group['rating']))
+    user_ratings[user_id] = ratings
+
+# Filter out movies for which both users have provided ratings
+# Assuming you have two specific users, UserA and UserB
+user_a_ratings = user_ratings.get(1, {})
+user_b_ratings = user_ratings.get(2, {})
+common_movies = set(user_a_ratings.keys()) & set(user_b_ratings.keys())
+
+# Create the map movie:(ratingUserA, ratingUserB) for common movies
+movie_ratings_map = {}
+for movie_id in common_movies:
+    movie_ratings_map[movie_id] = (user_a_ratings.get(movie_id), user_b_ratings.get(movie_id))
+
+print("\n\n")
+print(movie_ratings_map)
+
---
+++ recommendation-fairness/utils.py
@@ -0,0 +1,9 @@
+import numpy
+
+def pearson_correlation():
+    
+    
+    similarity = 0
+    return similarity
+
+
